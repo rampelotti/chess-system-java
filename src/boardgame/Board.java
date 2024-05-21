522b4d909f0d98df94046e37cc38ffbd35d708a8
Gabriel Rampelotti <gabrieelrampelotti@gmail.com>
2024-05-21 13:51:11 -0300

    Movendo peças

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -47,9 +47,21 @@ public class Board {
 		if(thereIsAPiece(position)) {
 			throw new BoardException("Já existe uma peça nessa posição!" + position);
 		}
-				
 		pieces[position.getRow()][position.getColumn()] = piece;
 		piece.position = position;
+	}
+	
+	public Piece removePiece(Position position) {
+		if (!positionExists(position)) {
+			throw new BoardException("Posição não existe no tabuleiro!");
+		}
+		if (piece(position) == null) {
+			return null;
+		}
+		Piece aux = piece(position);
+		aux.position = null;
+		pieces[position.getRow()][position.getColumn()] = null;
+		return aux;
 	}
 	
 	private boolean positionExists(int row, int column) {
