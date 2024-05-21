696ff1f4984ed6218b5b56254a848bcadc83758e
Gabriel Rampelotti <gabrieelrampelotti@gmail.com>
2024-05-21 14:18:42 -0300

    Limpando tela e possiveis movimentos da peça

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -41,6 +41,10 @@ public class ChessMatch {
 		if(!board.thereIsAPiece(position)) {
 			throw new ChessException("Não existe peça na posição de origem!");
 		}
+		if (!board.piece(position).isThereAnyPossibleMove()) {
+			throw new ChessException("Não existe movimento possivel!");
+			
+		}
 	}
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
 		board.placePiece(piece, new ChessPosition(column, row).toPosition());
